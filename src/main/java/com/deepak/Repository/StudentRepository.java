df9cda16fe9a5d75104ff4adc9485db5b9eb9bb9
deepakJdev <deepak.dev8950@gmail.com>
2026-05-09 14:08:51 +0530

    Update StudentRepository.java

--- src/main/java/com/deepak/Repository/StudentRepository.java
+++ src/main/java/com/deepak/Repository/StudentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.deepak.Entity.StudentEntity;
 
 public interface StudentRepository extends JpaRepository<StudentEntity,Integer> {
-
+// this interface will contains the customized repository methods
 }
